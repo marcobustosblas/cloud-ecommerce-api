3ec8ee218d90d4f074193636cd79bb928704e99f
Marco <114123687+marcobustosblas@users.noreply.github.com>
2026-05-05 17:09:28 -0400

    feat(order): implement JOIN FETCH to solve LazyInitializationException in findById

--- src/main/java/com/marco/cloud_ecommerce_api/infrastructure/persistence/jpa/adapter/OrderRepositoryAdapter.java
+++ src/main/java/com/marco/cloud_ecommerce_api/infrastructure/persistence/jpa/adapter/OrderRepositoryAdapter.java
@@ -5,6 +5,9 @@ import com.marco.cloud_ecommerce_api.domain.order.OrderRepository;
 import com.marco.cloud_ecommerce_api.infrastructure.persistence.jpa.entity.OrderJpaEntity;
 import com.marco.cloud_ecommerce_api.infrastructure.persistence.jpa.mapper.OrderMapper;
 import com.marco.cloud_ecommerce_api.infrastructure.persistence.jpa.repository.OrderJpaRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -17,6 +20,7 @@ public class OrderRepositoryAdapter implements OrderRepository {
 
     private final OrderJpaRepository jpaRepository;
     private final OrderMapper mapper;
+    private final EntityManager entityManager;
 
     @Override
     public Order save(Order order) {
@@ -27,12 +31,28 @@ public class OrderRepositoryAdapter implements OrderRepository {
 
     @Override
     public Optional<Order> findById(UUID id) {
-        return jpaRepository.findById(id).map(mapper::toDomain);
+        String jpql = "SELECT DISTINCT o FROM OrderJpaEntity o LEFT JOIN FETCH o.items WHERE o.id=:id";
+        TypedQuery<OrderJpaEntity> query = entityManager.createQuery(jpql, OrderJpaEntity.class);
+        query.setParameter("id", id);
+        try {
+            OrderJpaEntity entity = query.getSingleResult();
+            return Optional.of(mapper.toDomain(entity));
+        } catch (NoResultException e){
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<Order> findByIdempotentKey(String idempotentKey) {
-        return jpaRepository.findByIdempotentKey(idempotentKey).map(mapper::toDomain);
+        String jpql = "SELECT DISTINCT o FROM OrderJpaEntity o LEFT JOIN FETCH o.items WHERE o.idempotentKey=:key";
+        TypedQuery<OrderJpaEntity> query = entityManager.createQuery(jpql, OrderJpaEntity.class);
+        query.setParameter("key", idempotentKey);
+        try {
+            OrderJpaEntity entity = query.getSingleResult();
+            return Optional.of(mapper.toDomain(entity));
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
     }
 
 }
