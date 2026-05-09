8bd9179aa2f5b55d13ce8f2e779c646b4856070f
Marco <114123687+marcobustosblas@users.noreply.github.com>
2026-05-09 19:13:05 -0400

    feat(persistence): resolve N+1 problem using @EntityGraph

--- src/test/java/com/marco/cloud_ecommerce_api/infrastructure/persistence/jpa/NPlusOneTest.java
+++ src/test/java/com/marco/cloud_ecommerce_api/infrastructure/persistence/jpa/NPlusOneTest.java
@@ -17,7 +17,6 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
