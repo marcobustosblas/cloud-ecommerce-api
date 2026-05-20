ffaa1d468c73018000b886ac92137e84c379703c
Marco <114123687+marcobustosblas@users.noreply.github.com>
2026-05-20 19:31:19 -0400

    feat(application): add User DTOs and mapper

---
+++ src/main/java/com/marco/cloud_ecommerce_api/application/user/UserDtoMapper.java
@@ -0,0 +1,33 @@
+package com.marco.cloud_ecommerce_api.application.user;
+
+import com.marco.cloud_ecommerce_api.domain.user.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoMapper {
+
+    /* RequestDTO → Domain (para crear un nuevo usuario)
+    *  Nota: el service entrega el passwordHash
+    */
+    public User toDomain(UserRequestDTO request, String passwordHash) {
+        if (request == null) return null;
+        return new User(request.getEmail(), passwordHash);
+    }
+
+    /*
+     * Domain → ResponseDTO
+     * Excluye passwordHash y cartId por seguridad
+     */
+    public UserResponseDTO userResponseDTO(User user) {
+        if (user == null) return null;
+        return new UserResponseDTO(
+                user.getId(),
+                user.getEmail(),
+                user.getRoles(),
+                user.getStatus(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
+        );
+    }
+
+}
---
+++ src/main/java/com/marco/cloud_ecommerce_api/application/user/UserRequestDTO.java
@@ -0,0 +1,21 @@
+package com.marco.cloud_ecommerce_api.application.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequestDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 5, message = "Password must be al least 5 characters")
+    private String password;
+
+}
